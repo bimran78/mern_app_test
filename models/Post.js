2b6303ac09a46a048eb0110e9f70486a4e0d959f
burhanuddin Imran <bimran@digital-dividend.com>
2022-06-27 11:19:09 +0500

    first commit

---
+++ models/Post.js
@@ -0,0 +1,28 @@
+
+//  create Schema For todo as Post
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const PostSchema = new Schema({
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'user',
+    },
+   
+    description: {
+      type: String,
+      required: true,
+      
+    },
+  
+  
+    date: {
+        type: Date,
+        default: Date.now,
+      },
+    
+ } )
+
+ module.exports = Post = mongoose.model('post', PostSchema);
